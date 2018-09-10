1fa9e6335d7e30b922674ac26b8c3fa97d0b07e8
Mickel Jiang <mickel.jiang@gllue.com>
2018-09-10 09:03:49 +0800

    :star2: 完成测试器

--- IPProxyPool/tester.py
+++ IPProxyPool/tester.py
@@ -1,11 +1,12 @@
 """
 连接池测试器
 """
-import sys
+import time
 import asyncio
 import aiohttp
 from IPProxyPool.logger import logger
 from IPProxyPool.store import RedisClient
+from IPProxyPool.settings import BATCH_TEST_SIZE
 
 
 class Tester(object):
@@ -26,5 +27,23 @@ class Tester(object):
                 real_proxy = 'http://' + proxy
                 logger.info('正在测试代理: {}'.format(real_proxy))
             except Exception:
-                pass
-        pass
+                self.client.decrease(proxy)
+                logger.info('代理[{}]请求失败'.format(proxy))
+
+    def run(self):
+        """测试器主函数"""
+        logger.info("开始运行测试器")
+        try:
+            count = self.client.count()
+            logger.info("当前剩余 {} 个代理".format(count))
+            for i in range(0, count, BATCH_TEST_SIZE):
+                start = i
+                end = min(i + BATCH_TEST_SIZE, count)
+                logger.info("正在测试第 {}-{} 个代理".format(start+1, end))
+                test_proxies = self.client.batch(start, end)
+                loop = asyncio.get_event_loop()
+                tasks = [self.test_single_proxy(proxy) for proxy in test_proxies]
+                loop.run_until_complete(asyncio.wait(tasks))
+                time.sleep(5)
+        except Exception as e:
+            logger.error("测试器发生错误 {}".format(e.args))
